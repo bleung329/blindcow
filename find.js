fcfc56d330edd24802b3d189e6c9ffb75cba1fc7
bleung329 <bleung@stuy.edu>
2017-12-13 00:13:40 -0500

    it works now

--- find.js
+++ find.js
@@ -1,26 +1,50 @@
+/*
+Brian Leung
+pd 7 Softdev
+WORK 17 Moo?
+*/
 var box = document.getElementById("box");
 var boxHeight = box.offsetHeight;
 var boxWidth = box.offsetWidth;
 
 //hardcode target as center
 //randomize later
-var targetX = boxWidth / 2;
-var targetY = boxHeight / 2;
-
-
+var targetX = Math.abs(Math.trunc(Math.random(6)*boxWidth));
+var targetY = Math.abs(Math.trunc(Math.random(7)*boxHeight));
+console.log("target: "+targetX+","+targetY);
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-  /* YOUR IMPLEMENTATION */
+	return Math.sqrt(Math.pow(x1-x0,2)+Math.pow(y1-y0,2));
 };
 
+var maxDist = distance(0,0,targetX,targetY);	
+console.log("maxDist: " + maxDist)
 
-var findIt = function(e) {
-  /* YOUR IMPLEMENTATION */
+var findIt = function(e)
+{
+	console.log("x: "+e.clientX);
+	console.log("y: "+e.clientY);
+	//console.log("distance: "+distance(e.clientX,e.clientY,targetX,targetY));
+	var dust = distance(e.clientX,e.clientY,targetX,targetY);
+	var actDist = Math.trunc(255*(maxDist-dust)/maxDist);
+	console.log(rgb(actDist,actDist,actDist));
+	box.style.backgroundColor = rgb(actDist,actDist,actDist);
 };
 
+//Converts the actDist to a hexcode for color
+var rgb = function(r,g,b)
+{	
+	var newStr = r.toString(16);
+	if (newStr.length <2)
+	{
+		newStr="0"+newStr;
+	}
+
+	return("#"+newStr+newStr+newStr);
+};
 /*
 your OTHER FXNS
 
